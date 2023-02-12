c5a89cba526bdd90c7560c9d7f275fd68086e33c
Rivka Cohen <84354035+rivkacohe@users.noreply.github.com>
2023-02-13 01:23:37 +0200

    Add files via upload

---
+++ precache-manifest.d281b29f1999b311904fc3a48d9882be.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "85d167408e7c66e79a01",
+    "url": "/gemolav/static/css/main.08ea1b6d.chunk.css"
+  },
+  {
+    "revision": "85d167408e7c66e79a01",
+    "url": "/gemolav/static/js/main.85d16740.chunk.js"
+  },
+  {
+    "revision": "57fb75a2e8592a1edd30",
+    "url": "/gemolav/static/js/1.57fb75a2.chunk.js"
+  },
+  {
+    "revision": "3a45f3f779ea5aa9087d",
+    "url": "/gemolav/static/css/2.12b02955.chunk.css"
+  },
+  {
+    "revision": "3a45f3f779ea5aa9087d",
+    "url": "/gemolav/static/js/2.3a45f3f7.chunk.js"
+  },
+  {
+    "revision": "665be21f9eb68f28b888",
+    "url": "/gemolav/static/js/runtime~main.665be21f.js"
+  },
+  {
+    "revision": "7f477633ddd12f84284654f2a2e89b8a",
+    "url": "/gemolav/static/media/bootstrap-icons.7f477633.woff2"
+  },
+  {
+    "revision": "52196284de1fcb5b044f001a75482dba",
+    "url": "/gemolav/static/media/bootstrap-icons.52196284.woff"
+  },
+  {
+    "revision": "1543c4c6cde864d9d98714f30a161ec7",
+    "url": "/gemolav/index.html"
+  }
+];
